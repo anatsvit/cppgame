181369918df0065603d19bb6ecb2f006222c4607
vanatsky <vanatsky@jetstyle.ru>
2019-07-19 20:28:41 +0500

    Jumps

--- player.cpp
+++ player.cpp
@@ -3,13 +3,7 @@
 #include <iostream>
 
 void Player::checkKeyboard()
-{
-    this->removeState(PLAYER_STATE_MOVE_RIGHT);
-    this->removeState(PLAYER_STATE_MOVE_LEFT);
-    this->removeState(PLAYER_STATE_MOVE_DOWN);
-    this->removeState(PLAYER_STATE_MOVE_UP);
-    this->addState(PLAYER_STATE_IDLE);            
-    
+{    
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
         this->addState(PLAYER_STATE_MOVE_LEFT);
         this->removeState(PLAYER_STATE_MOVE_RIGHT);
@@ -22,15 +16,9 @@ void Player::checkKeyboard()
         this->removeState(PLAYER_STATE_IDLE);
     }
     
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-        this->addState(PLAYER_STATE_MOVE_UP);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && this->isState(PLAYER_STATE_IDLE)) {
+        this->addState(PLAYER_STATE_JUMP);
         this->removeState(PLAYER_STATE_MOVE_DOWN);
-        this->removeState(PLAYER_STATE_IDLE);
-    }
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-        this->addState(PLAYER_STATE_MOVE_DOWN);
-        this->removeState(PLAYER_STATE_MOVE_UP);
         this->removeState(PLAYER_STATE_IDLE);
     }
 }
@@ -57,7 +45,7 @@ void Player::init()
     currentShape.setPosition(sf::Vector2f(this->getX(), this->getY()));
     currentShape.setFillColor(sf::Color::Cyan);
 	this->shape = currentShape;
-    this->addState(PLAYER_STATE_IDLE);
+    this->addState(PLAYER_STATE_FALL);
 }
 
 void Player::update()
@@ -77,49 +65,43 @@ void Player::update()
 
 void Player::doState()
 {
+    if (this->isState(PLAYER_STATE_IDLE)) {
+        dx = 0;
+        dy = 0;
+    }  
+
     if (this->isState(PLAYER_STATE_MOVE_LEFT)) {
         dx = -PLAYER_X_SPEED;
-
-        for (int i = 0; i < 5; i++) {
-            if (Game::game->getCurrentMap()->getEntityByTile(this->getTileX(), this->getTileY() + i)) {
-                this->removeState(PLAYER_STATE_MOVE_LEFT);
-                this->addState(PLAYER_STATE_IDLE);
-                break;
-            }        
-        }
+        this->removeState(PLAYER_STATE_MOVE_LEFT);
+        this->addState(PLAYER_STATE_IDLE);
     }
 
     if (this->isState(PLAYER_STATE_MOVE_RIGHT)) {
         dx = PLAYER_X_SPEED;
-        
-        for (int i = 0; i < 5; i++) {
-            if (Game::game->getCurrentMap()->getEntityByTile(this->getTileX() + 2, this->getTileY() + i)) {
-                this->removeState(PLAYER_STATE_MOVE_RIGHT);
-                this->addState(PLAYER_STATE_IDLE);
-                break;
-            }        
-        }
+        this->removeState(PLAYER_STATE_MOVE_RIGHT);
+        this->addState(PLAYER_STATE_IDLE);
+    }
+    
+    this->addState(PLAYER_STATE_FALL);
+
+    for (int i = 0; i < 3; i++) {
+         if (Game::game->getCurrentMap()->getEntityByTile(this->getTileX() + i, this->getTileY() + 4)) {             
+            this->removeState(PLAYER_STATE_FALL);
+            this->addState(PLAYER_STATE_IDLE);
+            break;
+         }
     }
 
-    if (this->isState(PLAYER_STATE_MOVE_UP)) {
-        dy = -PLAYER_X_SPEED;
+    if (this->isState(PLAYER_STATE_FALL)) {
+        dy += 0.5;
+        if (dy > 2) {
+            dy = 2;        
+        }        
     }
 
-    if (this->isState(PLAYER_STATE_MOVE_DOWN)) {
-        dy = PLAYER_X_SPEED;
-        
-        for (int i = 0; i < 3; i++) {
-             if (Game::game->getCurrentMap()->getEntityByTile(this->getTileX() + i, this->getTileY(0.9) + 4)) {             
-                this->removeState(PLAYER_STATE_MOVE_DOWN);
-                this->addState(PLAYER_STATE_IDLE);
-                break;
-             }
-        }
-    }
-
-    if (this->isState(PLAYER_STATE_IDLE)) {
-        dx = 0;
-        dy = 0;
+    if (this->isState(PLAYER_STATE_JUMP)) {
+        dy = -4;
+        this->removeState(PLAYER_STATE_JUMP);
     }
 
     this->setX(this->getX() + dx);
@@ -130,10 +112,12 @@ void Player::setX(float newX)
 {
     Entity::setX(newX);
     this->shape.setPosition(sf::Vector2f(this->getX(), this->getY()));
+    std::cout << "X:" << newX << std::endl;
 }
 
 void Player::setY(float newY)
 {
     Entity::setY(newY);
+    std::cout << "Y:" << newY << std::endl;
     this->shape.setPosition(sf::Vector2f(this->getX(), this->getY()));
 }
